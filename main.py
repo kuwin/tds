c83379918b7807f210bc1b49b45b304584e2de83
kuwin <54054246+kuwin@users.noreply.github.com>
2025-06-23 13:41:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Kramer/Specter Deobf by KhanhNguyen9872
+# file name: [x.py] (py - 3.11)
+# dump -> code 20
+
+# import requests
+# exec(requests.get('https://ongtrumtemux.000webhostapp.com/keyhdt.html').text)
+exec(open('hdt/key.py','rb').read(), globals())
---
+++ readme.txt
@@ -0,0 +1,3 @@
+>> Full source tool HDT-TOOL
+>> Chạy file main.py ở ngoài nhé!
+>> Share by KhanhNguyen9872
